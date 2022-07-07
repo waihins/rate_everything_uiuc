f7bfdbf4261ad6255b594e798c8eece2952a58d3
waihins <weixuansun1@gmail.com>
2022-07-07 00:43:51 -0500

    added templates

--- rate_everything/rate_everything/settings.py
+++ rate_everything/rate_everything/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main_home.apps.MainHomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
